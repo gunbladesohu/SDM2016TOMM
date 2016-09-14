a1105c67cdb744243cb09c82430291abcb108a76
Tom <1678312165@qq.com>
2016-09-14 10:37:22 +0000

    creatdbtom

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,22 @@ Paper.create(user_id:'1',submitted_date:'08-09-2015',paper_status:'xxxx',moderat
 analyst_id:'1',analysed_time:'01-02-2013',authors:'xxx',title:'xxx',journal:'xxx',publish_year:'2013',publish_volume:'13',
 publish_pages:'13',publish_month:'Dec',credibility_rate:'xx',research_metrix:'xx',research_aim:'xxx',research_method_field1:'true',
 research_method_field2:'true',research_method_field3:'true',research_method_field4:'false',participant1:'true',
-participant2:'true',participant3:'false',participant4:'true')
+participant2:'true',participant3:'false',participant4:'true')  
+Methodology.create(methodology_name:"Scrum",description:"xxx")
+Methodology.create(methodology_name:"Waterfall",description:"xxx")
+Methodology.create(methodology_name:"Spiral",description:"xxx")
+Methodology.create(methodology_name:"XP",description:"xxx")
+Methodology.create(methodology_name:"Rational Unified Process",description:"xxx")
+Methodology.create(methodology_name:"Crystal",description:"xxx")
+Methodology.create(methodology_name:"Clean room",description:"xxx")
+Methodology.create(methodology_name:"Feature Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Model Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Domain Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Formal methods",description:"xxx")
+Methodology.create(methodology_name:"Problem Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Cloud computing",description:"xxx")
+Methodology.create(methodology_name:"Service Oriented Development",description:"xxx")
+Methodology.create(methodology_name:"Aspect Oriented Development",description:"xxx")
+Methodology.create(methodology_name:"Valuse Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Product Driven Development",description:"xxx")
+Methodology.create(methodology_name:"Agile",description:"xxx")
