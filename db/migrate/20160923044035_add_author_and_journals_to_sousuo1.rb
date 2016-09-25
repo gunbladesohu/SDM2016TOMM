44bb04eaf84c19bcc573d7a05c7f52c6c0bd84b1
ChenMo <gunbladesohu@gmail.com>
2016-09-25 10:16:24 +0000

    merge with seach_back

--- db/migrate/20160923044035_add_author_and_journals_to_sousuo1.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorAndJournalsToSousuo1 < ActiveRecord::Migration[5.0]
-  def change
-    add_column :sousuo1s, :authors, :string
-    add_column :sousuo1s, :journal, :string
-  end
-end
