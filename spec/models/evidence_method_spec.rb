0a48b2fff6c7a396a8fadf4b2af8e5fa6bece0b0
ChenMo <gunbladesohu@gmail.com>
2016-10-19 02:41:36 +0000

    repair evidence method

--- spec/models/evidence_method_spec.rb
+++ spec/models/evidence_method_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe EvidenceevienceMethod, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#RSpec.describe EvidenceMethod, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
